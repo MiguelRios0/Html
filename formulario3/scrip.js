66355e2f908144a3f76a26697aa16bcf420969e6
miguel <malfonsorios@ucundinamarca.edu.co>
2026-03-29 14:30:34 -0500

    Update scrip.js

--- formulario3/scrip.js
+++ formulario3/scrip.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const formulario = document.querySelector("form");
+
+    formulario.addEventListener("submit", function (event) {
+
+        let confirmacion = confirm("¿Seguro que deseas enviar el formulario?");
+
+        if (!confirmacion) {
+            event.preventDefault();
+        } else {
+            alert("Formulario enviado correctamente");
+        }
+
+    });
+
+});
 
